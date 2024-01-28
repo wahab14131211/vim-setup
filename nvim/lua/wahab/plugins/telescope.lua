bacceae7e6f87907c8c91fa47dae2e70fdc33364
Wahab <wahabata@gmail.com>
2024-01-28 02:21:33 -0500

    add remap to telescope to search help menus

--- nvim/lua/wahab/plugins/telescope.lua
+++ nvim/lua/wahab/plugins/telescope.lua
@@ -7,8 +7,9 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fs', function()
+        vim.keymap.set('n', '<leader>fg', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") });
         end)
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     end
 }
